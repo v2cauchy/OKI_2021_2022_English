5d19c0943cd8cc83d9f817963e8246eda7181d72
v2cauchy <102176805+v2cauchy@users.noreply.github.com>
2022-03-29 19:39:38 +0200

    v1.4 push() method

--- mqueue.cpp
+++ mqueue.cpp
@@ -2,12 +2,13 @@
 //class implementing queue in C++
 //the same functionality as STL (Standard Template Library ) class queue in C++ 
 //with usage example
-//Version 1.3
+//Version 1.4
 Consists
 a. Program template
 b. defintion of class mqueue 
 c. Constructor
 d. empty() method
+e. push() method
 Author: Daniel Olkowski / Olimpijskie Kolo Informatyczne
  */
  
@@ -25,7 +26,16 @@ public:
         return true; 
     return false; 
  }	
- 
+ void push(int value) {
+    if (  empty() == true ) {
+       elements.push_back(value);
+	   first = last = 0;
+	   return;
+    }
+    elements.push_back(value);
+    ++last;
+ }	
+
 private:
  vector<int> elements;
  int first, last;
@@ -34,7 +44,9 @@ private:
 int main() {
  mqueue	payments;
  
- cout << "empty?: " << payments.empty() << "\n"; 
+ cout << "a1: empty?: " << payments.empty() << "\n"; 
+  payments.push(5); 
+ cout << "a2: empty?: " << payments.empty() << "\n"; 
 
  return 0;
 }
