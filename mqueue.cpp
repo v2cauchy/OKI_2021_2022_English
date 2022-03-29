bf5bcb35696a5021c6d498ea8e49d3e950caa097
v2cauchy <102176805+v2cauchy@users.noreply.github.com>
2022-03-29 19:27:06 +0200

    v1.3 empty() method

--- mqueue.cpp
+++ mqueue.cpp
@@ -2,11 +2,12 @@
 //class implementing queue in C++
 //the same functionality as STL (Standard Template Library ) class queue in C++ 
 //with usage example
-//Version 1.2
+//Version 1.3
 Consists
 a. Program template
 b. defintion of class mqueue 
 c. Constructor
+d. empty() method
 Author: Daniel Olkowski / Olimpijskie Kolo Informatyczne
  */
  
@@ -19,6 +20,12 @@ public:
  mqueue() { 
     first = last = -1;
  } 
+ bool empty() {
+    if (first == -1 ) 
+        return true; 
+    return false; 
+ }	
+ 
 private:
  vector<int> elements;
  int first, last;
@@ -26,7 +33,8 @@ private:
 
 int main() {
  mqueue	payments;
- mqueue	payments2;
+ 
+ cout << "empty?: " << payments.empty() << "\n"; 
 
  return 0;
 }
