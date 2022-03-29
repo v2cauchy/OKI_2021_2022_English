31a3a1aa9ec45b0719bcd8228d49eb383eec5163
v2cauchy <102176805+v2cauchy@users.noreply.github.com>
2022-03-29 18:43:51 +0200

    v1.0 Template of program

---
+++ mqueue.cpp
@@ -0,0 +1,18 @@
+/*
+//class implementing queue in C++
+//the same functionality as STL (Standard Template Library ) class queue in C++ 
+//with usage example
+//Version 1.0
+Consists
+a. Program template
+Author: Daniel Olkowski / Olimpijskie Kolo Informatyczne
+ */
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+ return 0;
+}
